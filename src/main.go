89749730e291e14b899d3b0bf1ff6dfebb267b23
Alexander G <alexander.maverick1996@gmail.com>
2022-02-02 01:11:32 -0300

    third commit based in declare variables

--- src/main.go
+++ src/main.go
@@ -7,4 +7,10 @@ func main() {
 	const pi float64 = 3.1416
 	const pi2 float64 = 3.14163
 	fmt.Println(pi, pi2)
+
+	// Declaración de variables
+	base := 12
+	var altura int = 14
+	var area int
+	fmt.Println(base, altura, area)
 }
