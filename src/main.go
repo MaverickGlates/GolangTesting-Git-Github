3d6ee891e0c997d489a27c01cb67affa47826df8
Alexander G <alexander.maverick1996@gmail.com>
2022-02-02 01:23:12 -0300

    commit 4

--- src/main.go
+++ src/main.go
@@ -13,4 +13,18 @@ func main() {
 	var altura int = 14
 	var area int
 	fmt.Println(base, altura, area)
+
+	// Zero value
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//area de un cuadrado
+	const squarebase = 10
+	squareArea := squarebase * squarebase
+	fmt.Println("Área Cuadrado:", squareArea)
+
 }
